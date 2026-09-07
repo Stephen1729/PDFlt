fcad9e21c8cdaf7f190dca1cce6336d3d48311c8
Ludwing <265607217+Stephen1729@users.noreply.github.com>
2026-09-07 14:37:25 -0500

    chore: bump version to 1.1.0

--- src/renderer/src/views/settings.ts
+++ src/renderer/src/views/settings.ts
@@ -1,5 +1,5 @@
 export async function renderSettings(container: HTMLElement): Promise<void> {
-  const version = '1.0.0'
+  const version = '1.1.0'
 
   container.innerHTML = `
     <div class="view-header" style="justify-content: center;">
